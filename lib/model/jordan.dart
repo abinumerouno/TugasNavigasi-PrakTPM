64a1dae4fc249944899dee44a92fea778b2b9b04
abinumerouno <abigailrakhabhagaskara@gmail.com>
2022-03-14 11:25:24 +0700

    123190144 AbigailRB

---
+++ lib/model/jordan.dart
@@ -0,0 +1,44 @@
+class Jordan {
+  String image;
+  String name;
+  int price;
+
+  Jordan({
+    required this.image,
+    required this.name,
+    required this.price,
+  });
+}
+
+var getData = [
+  Jordan(
+    image:
+    "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/1e081669-e74d-41f5-b422-6f307d02f44d/air-jordan-1-mid-paris-mens-shoes-n65720.png",
+    name: "AIR JORDAN 1 MID PARIS",
+    price: 1937000,
+  ),
+  Jordan(
+    image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/4a2604c8-420c-455a-b6f8-e2aef7ea625e/jordan-6-17-23-shoes-kN83hh.png",
+    name: "JORDAN 6-17-23",
+    price: 2235000,
+  ),
+  Jordan(
+    image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/1f1b7246-1dcd-43de-af62-995320e977ef/air-jordan-6-retro-low-mens-shoes-MPwhfh.png",
+    name: "JORDAN 6 RETRO LOW",
+    price: 2980000,
+  ),
+  Jordan(
+    image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/fb2d0ea9-2665-42af-8581-fd1be4e301ff/air-jordan-xxxvi-psychic-energy-basketball-shoes-1D0pp7.png",
+    name: "AIR JORDAN XXXVI PSYCHIC ENERGY",
+    price: 2756500,
+  ),
+  Jordan(
+    image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/e635b87d-7240-4b45-b186-ef40ab6850ee/zion-1-basketball-shoes-bJ0hLJ.png",
+    name: "ZION 1",
+    price: 1788000,
+  ),Jordan(
+    image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/5b734c61-39ab-44b2-b18d-53963181f6d8/jordan-air-nfh-shoes-Ktzw9P.png",
+    name: "JORDAN AIR NFH",
+    price: 1639000,
+  ),
+];
